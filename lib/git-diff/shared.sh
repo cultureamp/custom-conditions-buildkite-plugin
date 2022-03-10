b471a2f9366a928eb386113ad68121a18805d793
Tobias Gerschner <tobias.gerschner@cultureamp.com>
2022-03-10 15:41:54 +1300

    add leading slash for ssm param prefix

--- lib/git-diff/shared.sh
+++ lib/git-diff/shared.sh
@@ -9,5 +9,5 @@ test -n "${BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_SCRIPT_PATH:-}" || { echo "script-
 test -x "${BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_SCRIPT_PATH:-}" || { echo "script-path not found or invalid" ; exit 1;}
 test -n "${BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_SSM_PREFIX:-}" || { echo "ssm-prefix option must be provided" ; exit 1;}
 
-ssm_parameter_name="$BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_SSM_PREFIX/$BUILDKITE_PIPELINE_SLUG/$BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_GIT_PATH"
+ssm_parameter_name="/$BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_SSM_PREFIX/$BUILDKITE_PIPELINE_SLUG/$BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_GIT_PATH"
 ssm_parameter_name="${ssm_parameter_name%%/}"
