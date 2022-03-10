030b164b1ef62b4609b090a0f76c41ae583e7072
Tobias Gerschner <tobias.gerschner@cultureamp.com>
2022-03-10 17:36:58 +1300

    some docs

--- lib/git-diff/post-command.sh
+++ lib/git-diff/post-command.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 set -xeuo pipefail
-test -n ""
 
+# if the meta-data is not set, the cache condition was missed
+# and we want to set it to the current git commit
 buildkite-agent meta-data get "custom-condition-$BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_NAME" || \
   aws ssm put-parameter \
     --name "/$git_cache_reference" \
