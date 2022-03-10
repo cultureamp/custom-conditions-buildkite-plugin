8bba4a94a515d6a7545e98c4539ae49a1c39b5cf
Tobias Gerschner <tobias.gerschner@cultureamp.com>
2022-03-10 17:32:57 +1300

    fix meta-data cmd in post hook

--- lib/git-diff/post-command.sh
+++ lib/git-diff/post-command.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 set -xeuo pipefail
+test -n ""
 
-buildkite-agent meta-data set "custom-condition-$BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_NAME" || \
+buildkite-agent meta-data get "custom-condition-$BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_NAME" || \
   aws ssm put-parameter \
     --name "/$git_cache_reference" \
     --type "String" \
