742f152022821c74c43249430aa7635a15513520
Tobias Gerschner <tobias.gerschner@cultureamp.com>
2022-03-11 22:16:37 +1300

    enable configurable debugging

--- lib/git-diff/shared.sh
+++ lib/git-diff/shared.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-set -x
+set -euo pipefail
+[ "${BUILDKITE_PLUGIN_DEBUG:-false}" == "true" ] && set -x ||:
 
 test -n "${BUILDKITE_COMMIT:-}" || { echo "BUILDKITE_COMMIT environment variable must be provided" ; exit 1;}
 test -n "${BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_NAME:-}" || { echo "condition name must be provided" ; exit 1;}
@@ -13,5 +14,3 @@ git_cache_reference="$BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_CACHE_PREFIX/$BUILDKITE
 git_cache_reference="${git_cache_reference%%/}"
 
 export git_cache_reference
-
-set +x
