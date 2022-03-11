8e713358c1152e9ba5517660be0417c0b26dfe0b
Tobias Gerschner <tobias.gerschner@cultureamp.com>
2022-03-11 22:00:00 +1300

    refine plugin spec and extend it for tags

--- lib/git-diff/shared.sh
+++ lib/git-diff/shared.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+set -x
 
 test -n "${BUILDKITE_COMMIT:-}" || { echo "BUILDKITE_COMMIT environment variable must be provided" ; exit 1;}
 test -n "${BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_NAME:-}" || { echo "condition name must be provided" ; exit 1;}
 test -n "${BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_CACHE_PREFIX:-}" || { echo "cache-prefix option must be provided" ; exit 1;}
 test -n "${BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_GIT_PATH:-}" || BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_GIT_PATH="."
 
+test -n "${BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_TAGS:-}" ||:
+
 git_cache_reference="$BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_CACHE_PREFIX/$BUILDKITE_PIPELINE_SLUG/$BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_GIT_PATH"
 git_cache_reference="${git_cache_reference%%/}"
 
 export git_cache_reference
+
+set +x
