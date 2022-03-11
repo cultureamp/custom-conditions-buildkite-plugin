92f38cc32374fbf099ce43c34f9e9fe28fdfc2f8
Tobias Gerschner <tobias.gerschner@cultureamp.com>
2022-03-12 00:17:19 +1300

    small refactor of post cmd

--- lib/git-diff/post-command.sh
+++ lib/git-diff/post-command.sh
@@ -3,21 +3,23 @@
 set -euo pipefail
 [ "${BUILDKITE_PLUGIN_DEBUG:-false}" == "true" ] && set -x ||:
 
-# if the meta-data is not set, the cache condition was missed
-# and we want to set it to the current git commit
-buildkite-agent meta-data get "custom-condition-$BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_NAME" || \
+update_ssm_param() {
   aws ssm put-parameter \
     --name "/$git_cache_reference" \
     --type "String" \
     --overwrite \
     --value "$BUILDKITE_COMMIT"
 
-if [ -n "${ssm_tag_params:-}" ] ; then
-  aws ssm add-tags-to-resource \
-      --resource-type "Parameter" \
-      --resource-id "/$git_cache_reference" \
-      --tags $ssm_tag_params
-else
-  /bin/true
-fi
+  if [ -n "${ssm_tag_params:-}" ] ; then
+    aws ssm add-tags-to-resource \
+        --resource-type "Parameter" \
+        --resource-id "/$git_cache_reference" \
+        --tags $ssm_tag_params
+  else
+    /bin/true
+  fi
+}
 
+# if the meta-data is not set, the cache condition was missed
+# and we want to set it to the current git commit
+buildkite-agent meta-data get "custom-condition-$BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_NAME" || update_ssm_param
