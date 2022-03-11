37e87822914c7e968b9d8e3868bdff271169c3e6
Tobias Gerschner <tobias.gerschner@cultureamp.com>
2022-03-11 23:02:12 +1300

    pull in helper script

--- lib/shared.sh
+++ lib/shared.sh
@@ -3,11 +3,14 @@
 set -euo pipefail
 [ "${BUILDKITE_PLUGIN_DEBUG:-false}" == "true" ] && set -x ||:
 
-curr_dir="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+# shellcheck source=lib/helpers.sh
+. "$DIR/../lib/helpers.sh"
 
 condition_type="${BUILDKITE_PLUGIN_CUSTOM_CONDITIONS_TYPE:-}"
-condition_script="$curr_dir/../lib/${condition_type}/${PLUGIN_STAGE}.sh"
-shared_script="$curr_dir/../lib/${condition_type}/shared.sh"
+condition_script="$DIR/../lib/${condition_type}/${PLUGIN_STAGE}.sh"
+shared_script="$DIR/../lib/${condition_type}/shared.sh"
 
 test -e "$condition_script" || { echo "condition not supported in ${PLUGIN_STAGE}: $condition_type" ; exit 1 ;}
 test ! -e "$shared_script" || . "$shared_script"
